2b79fd1c14d1a6ef674074a39401a8e2ae28fcbb
jwhopk29 <jeremy.hopkins@wwt.com>
2023-05-24 11:53:38 -0500

    First commit

--- test_playwright.py
+++ test_playwright.py
@@ -2,17 +2,25 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
+
+    # page timeout
+    # page.set_default_timeout(30000)
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    page.wait_for_load_state("networkidle")
     page.get_by_role("button", name="Log In").click()
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_role("button", name="Log in with Email").click()
     page.get_by_test_id("emailAuth").get_by_label("Email").fill("symon.storozhenko@gmail.com")
     page.get_by_test_id("emailAuth").get_by_label("Email").press("Tab")
     page.get_by_label("Password").fill("test123")
-    page.get_by_label("Password").press("Enter")
+
+    #timeout for particular element
+    page.get_by_label("Password").press("Enter", timeout=10000)
+    
+    # expect(page.get_by_role("button", name="Log In")).to_be_hidden()
 
     # ---------------------
     context.close()
